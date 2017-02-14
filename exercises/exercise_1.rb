76f524119e1ceee432cc41f0049228810dffe8c3
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-14 22:09:28 +0000

    Added table references to classes, created employee instances

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,10 +5,6 @@ puts "----------"
 
 # Your code goes below here ...
 
-class Store < ActiveRecord::Base
-
-end
-
 Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
 Store.create(name: 'Richmond', annual_revenue: 300000, mens_apparel: false, womens_apparel: true )
 Store.create(name: 'Gastown', annual_revenue: 300000, mens_apparel: true, womens_apparel: false )
