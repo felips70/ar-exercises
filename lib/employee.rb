a42409b00d3ffbf0377e8523a0f76823fba3dad8
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-14 22:36:01 +0000

    employee validation complete

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates :first_name, :last_name, presence: true
+  validates_numericality_of :hourly_rate, :greater_than_or_equal_to => 40
+  validates_numericality_of :hourly_rate, :less_than_or_equal_to => 200
+  validates_associated :store
+
 end
